448edf45565c9c7e0bcc708ff60622ba1b612edd
francoisvercellotti <francoisvercellotti@gmail.com>
2025-01-14 14:50:41 +0100

    Ajout du workflow CI pour tests et documentation

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
 def main():
+    """
+    La fonction principale du programme qui imprime un message.
+
+    Cette fonction est exécutée lorsqu'on lance le programme,
+    et elle affiche "Hello from bi-intelligence-project!" dans la console.
+    """
     print("Hello from bi-intelligence-project!")
 
 
